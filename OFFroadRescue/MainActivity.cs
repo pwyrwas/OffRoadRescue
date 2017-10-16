03c1d153f1bed3e0904852c7ba2b6e812b3028f7
pwyrwas <kudlaty951@gmail.com>
2017-10-16 21:36:08 +0200

    Add project files.

---
+++ OFFroadRescue/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Runtime;
+using Android.OS;
+using Android.Widget;
+using System;
+
+namespace OFFroadRescue
+{
+    [Activity(Label = "OFFroadRescue", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private Button mBtnSignUp;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            mBtnSignUp = FindViewById<Button>(Resource.Id.btnSignUp);
+
+            mBtnSignUp.Click += (object sender, EventArgs args) =>
+            {
+                //Pull up dialog
+                FragmentTransaction transaction = FragmentManager.BeginTransaction();
+                dialog_SignUp signUpDialog = new dialog_SignUp();
+                signUpDialog.Show(transaction, "dialog fragment");
+            };
+        }
+    }
+}
+
---
+++ OFFroadRescue/dialog_SignUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace OFFroadRescue
+{
+
+    class dialog_SignUp : DialogFragment
+    {
+        private EditText mtxtFirstName;
+        private EditText mTxtEmail;
+        private EditText mTxtPassword;
+        private Button mBtnSignUp;
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+
+            var view = inflater.Inflate(Resource.Layout.dialog_sign_up, container, false);
+
+            mtxtFirstName = view.FindViewById<EditText>(Resource.Id.txtFirstName);
+            mTxtEmail = view.FindViewById<EditText>(Resource.Id.txtEmail);
+            mBtnSignUp = view.FindViewById<Button>(Resource.Id.btnDialogEmail);
+            mTxtPassword = view.FindViewById<EditText>(Resource.Id.txtPassword);
+
+            mBtnSignUp.Click += mBtnSignUp_Click;
+
+            return view;
+        }
+        void mBtnSignUp_Click(object sender, EventArgs e)
+        {
+            //User has clicked the sign up button 
+        }
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            Dialog.Window.RequestFeature(WindowFeatures.NoTitle); //Sets the tite bar to invisible
+            base.OnActivityCreated(savedInstanceState);
+            Dialog.Window.Attributes.WindowAnimations = Resource.Style.dialog_animation; // set the animation
+        }
+    }
+}
